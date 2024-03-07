6df37f28877cd297322dae0435c8f3e570e48bb5
nyotnoti08 <50961779+nyotnoti08@users.noreply.github.com>
2024-03-07 20:25:13 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,13 @@ import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 
 const initialContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+ { id: 'id-1', name: 'Edna Mode', number: '200-40-02' },
+ { id: 'id-2', name: 'Wednesday Addams', number: '199-11-993' },
+ { id: 'id-3', name: 'Wanda Maximoff', number: '645-17-79' },
+ { id: 'id-4', name: 'Captain Jack Sparrow', number: '227-91-26' },
+ { id: 'id-5', name: 'Steve Rogers', number: '201-14-01' },
+ { id: 'id-6', name: 'Ace Ventura', number: '649-98-132' },
+ { id: 'id-7', name: 'Bruce Lee', number: '998-82-123' },
 ];
 
 export const App = () => {
